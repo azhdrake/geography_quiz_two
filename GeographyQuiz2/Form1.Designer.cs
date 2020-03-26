e686b50544b45bfe6e8fa0c63f9633b331c25adb
B33 <my@email.com>
2020-03-26 14:52:28 -0500

    Add project files.

---
+++ GeographyQuiz2/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace GeographyQuiz2
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      this.txtAnswer = new System.Windows.Forms.TextBox();
+      this.btnSubmit = new System.Windows.Forms.Button();
+      this.lblQuestion = new System.Windows.Forms.Label();
+      this.timer1 = new System.Windows.Forms.Timer(this.components);
+      this.lblTime = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // txtAnswer
+      // 
+      this.txtAnswer.Location = new System.Drawing.Point(42, 171);
+      this.txtAnswer.MaxLength = 15;
+      this.txtAnswer.Name = "txtAnswer";
+      this.txtAnswer.Size = new System.Drawing.Size(189, 31);
+      this.txtAnswer.TabIndex = 0;
+      // 
+      // btnSubmit
+      // 
+      this.btnSubmit.Location = new System.Drawing.Point(262, 169);
+      this.btnSubmit.Name = "btnSubmit";
+      this.btnSubmit.Size = new System.Drawing.Size(106, 40);
+      this.btnSubmit.TabIndex = 1;
+      this.btnSubmit.Text = "Submit";
+      this.btnSubmit.UseVisualStyleBackColor = true;
+      this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+      // 
+      // lblQuestion
+      // 
+      this.lblQuestion.Location = new System.Drawing.Point(35, 41);
+      this.lblQuestion.Name = "lblQuestion";
+      this.lblQuestion.Size = new System.Drawing.Size(332, 128);
+      this.lblQuestion.TabIndex = 2;
+      this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+      // 
+      // timer1
+      // 
+      this.timer1.Enabled = true;
+      this.timer1.Interval = 1000;
+      this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+      // 
+      // lblTime
+      // 
+      this.lblTime.AutoSize = true;
+      this.lblTime.Location = new System.Drawing.Point(301, 238);
+      this.lblTime.Name = "lblTime";
+      this.lblTime.Size = new System.Drawing.Size(66, 25);
+      this.lblTime.TabIndex = 3;
+      this.lblTime.Text = "00:00";
+      this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(388, 285);
+      this.Controls.Add(this.lblTime);
+      this.Controls.Add(this.lblQuestion);
+      this.Controls.Add(this.btnSubmit);
+      this.Controls.Add(this.txtAnswer);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblTime;
+    }
+}
+
---
+++ GeographyQuiz2/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GeographyQuiz2
+{
+  public partial class Form1 : Form
+  {
+    private DateTime startTime = DateTime.Now;
+
+    // the questions and answers
+    private readonly SortedList<string, string> Questions = new SortedList<string, string>()
+    {
+      { "What is the state capital of California?", "Sacramento" },
+      { "What is the tallest mountain in the world?", "Mauna Kea" },
+      { "Which country has the furthest south extent", "Chile" }
+    };
+    // a place to store the questions answered incorrectally so that they might be displayed at the end.
+    private readonly SortedList<string, string> WrongAnswers = new SortedList<string, string>()
+    { };
+    private int currentQuestion = 0;
+
+    public Form1()
+    {
+      InitializeComponent();
+      getQuestion();
+    }
+
+    private void timer1_Tick(object sender, EventArgs e)
+    {
+      // The timer. Works by subtracting the current time from the time the app was opened once a second. 
+      DateTime currentTime = DateTime.Now;
+      TimeSpan timeTaken = currentTime.Subtract(startTime);
+
+      lblTime.Text = timeTaken.ToString(@"mm\:ss");
+    }
+
+    private void btnSubmit_Click(object sender, EventArgs e)
+    {
+      checkAnswer();
+      getQuestion();
+    }
+
+    private void checkAnswer()
+    {
+      if (currentQuestion < 2)
+      {
+        KeyValuePair<string, string> question = Questions.ElementAt(currentQuestion);
+        string answer = txtAnswer.Text.ToLower();
+
+        // I decided to be nice and take any answer that contains the correct answer BUT I also made a character limit of 15 for the answer text box so you can't just type nine million things in there.
+        if (!answer.Contains(question.Value.ToLower()))
+        {
+          WrongAnswers.Add(question.Key, question.Value);
+        }
+        txtAnswer.Text = "";
+        currentQuestion++;
+      }
+      else
+      {
+        endQuiz();
+      }
+    }
+
+    private void getQuestion()
+    {
+      if (currentQuestion < 3)
+      {
+        KeyValuePair<string, string> question = Questions.ElementAt(currentQuestion);
+        lblQuestion.Text = question.Key;
+      }
+    }
+    
+    private void endQuiz()
+    {
+      string corrections ="";
+      foreach(KeyValuePair<string, string> question in WrongAnswers)
+      {
+        corrections += ($"The correct answer to \"{question.Key}\" is \"{question.Value}\"\n");
+      }
+      int score = Questions.Count - WrongAnswers.Count;
+      MessageBox.Show($"Your score is {score}!\n{corrections}\nFinal Time: {lblTime.Text}");
+      timer1.Stop();
+    }
+
+  }
+}
